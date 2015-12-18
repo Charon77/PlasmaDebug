0f54a478a4d4751bf7a3e07ebc3fb6dc7b401528
Charon77 <evans.jahja@yahoo.com>
2015-12-18 23:03:06 +0700

    Trying to figure out how program should be structured.

---
+++ PlasmaDebug/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlasmaDebug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hi.");
+
+            ProcessStartInfo _gdbStartInfo = new ProcessStartInfo();
+            _gdbStartInfo.FileName = "gdb.exe";
+            _gdbStartInfo.UseShellExecute = false; //Why?
+            _gdbStartInfo.RedirectStandardOutput = true;
+            _gdbStartInfo.RedirectStandardInput = true;
+            _gdbStartInfo.RedirectStandardError = true;
+            _gdbStartInfo.CreateNoWindow = true;
+            Process p = Process.Start(_gdbStartInfo);
+
+            StreamReader s = p.StandardOutput;
+
+            string str = s.ReadLine();
+            Console.WriteLine(str);
+
+
+
+
+        }
+    }
+}
